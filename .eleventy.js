bfdefde9258ed70c0c5dceb7665642c6e9624aa3
Salwa <124178747+Soofmaax@users.noreply.github.com>
2025-07-23 10:37:15 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,25 +3,9 @@
  * FICHIER DE CONFIGURATION POUR ELEVENTY
  * ==========================================================================
  * Ce fichier contrôle la manière dont Eleventy construit le site.
- * Il définit les dossiers à copier, les bibliothèques à utiliser, etc.
  */
 
-// Importation du plugin sitemap pour la génération automatique du plan du site.
-const sitemap = require("@11ty/eleventy-plugin-sitemap");
-
 module.exports = function(eleventyConfig) {
-
-  // --------------------------------------------------------------------------
-  // PLUGINS
-  // --------------------------------------------------------------------------
-  // On active ici les plugins qui ajoutent des fonctionnalités à Eleventy.
-
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://www.bmsventouse.fr",
-    },
-  });
-
 
   // --------------------------------------------------------------------------
   // PASSTHROUGH FILE COPY
@@ -43,11 +27,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
   eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
   eleventyConfig.addPassthroughCopy("robots.txt");
-
-  // Fichiers de configuration pour l'hébergement (au cas où)
-  // Bien que nous utilisions netlify.toml, les garder ici ne coûte rien.
-  eleventyConfig.addPassthroughCopy("_headers");
-  eleventyConfig.addPassthroughCopy("_redirects");
 
 
   // --------------------------------------------------------------------------
